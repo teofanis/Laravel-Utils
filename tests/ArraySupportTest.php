22ac56c0bdb12daeba9051ba3f3da34bf799d2ac
Teofanis Papadopulos <teos_97@hotmail.com>
2021-08-07 20:31:38 +0100

    placeholder Test

--- tests/ArraySupportTest.php
+++ tests/ArraySupportTest.php
@@ -4,4 +4,9 @@ namespace Teofanis\LaravelUtils\Tests;
 
 class ArraySupportTest extends TestCase
 {
+    /** @test */
+    public function test_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
 }
