1dd3d3fcb4f00a3f15efe560809d830f765a072b
Teofanis Papadopulos <teos_97@hotmail.com>
2021-08-08 20:40:09 +0100

    some generic helper additions

--- src/Concerns/GenericHelpers.php
+++ src/Concerns/GenericHelpers.php
@@ -3,11 +3,64 @@
 namespace Teofanis\LaravelUtils\Concerns;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Blade;
 
 trait GenericHelpers {
 
-    function devMode($additonalDevEnvs = []) {
+    function devMode($additonalDevEnvs = [])
+    {
         $additonalDevEnvs = array_merge($additonalDevEnvs, ['local', 'staging', 'testing']);
         return App::environment(...$additonalDevEnvs);
+    }
+
+    function bladeCompile($someRandomViewName, array $args = [])
+    {
+        $compiled = Blade::compileString($someRandomViewName);
+        ob_start() and extract($args, EXTR_SKIP);
+
+        try {
+            eval('?>' . $compiled);
+        } catch (\Exception $e) {
+            ob_get_clean();
+            throw $e;
+        }
+        $content = ob_get_clean();
+        return $content;
+    }
+
+    function hydrateAndCompile($subject, $context = [])
+    {
+        $matches = [];
+        $new = $subject;
+        $count = preg_match_all('/\{\$(.*)\}/U', $subject, $matches);
+        if ($count && $matches && $context) {
+            $matchText = $matches[0];
+            $matchVar = $matches[1];
+            for ($i = 0; $i < $count; $i++) {
+                $txt = $matchText[$i];
+                $var = $matchVar[$i];
+                $val = $context[$var] ?? '';
+                $new = str_replace($txt, $val, $new);
+            }
+        }
+        return $this->bladeCompile($new, $context);
+    }
+
+    function dumpToString(...$args)
+    {
+        $orgVarDumperValue = $_SERVER['VAR_DUMPER_FORMAT'] ?? null;
+        $_SERVER['VAR_DUMPER_FORMAT']='html';
+
+        ob_start();
+        dump(...$args);
+        $content = ob_get_contents();
+        ob_get_clean();
+
+        if ($orgVarDumperValue === null) {
+            unset($_SERVER['VAR_DUMPER_FORMAT']);
+        } else {
+            $_SERVER['VAR_DUMPER_FORMAT']=$orgVarDumperValue;
+        }
+        return $content;
     }
 }
--- tests/GenericHelpersTest.php
+++ tests/GenericHelpersTest.php
@@ -2,11 +2,27 @@
 
 namespace Teofanis\LaravelUtils\Tests;
 
+use Teofanis\LaravelUtils\Facades\Utils;
+
 class GenericHelpersTest extends TestCase
 {
     /** @test */
-    public function test_true_is_true()
+    public function test_compile_html_with_blade()
     {
-        $this->assertTrue(true);
+        $html = '<a href="{!!$link!!}">{{$linkText}}</a>';
+        $compiled = Utils::bladeCompile($html,['link' => 'https://google.com/', 'linkText' => 'here']);
+        $actual = '<a href="https://google.com/">here</a>';
+        $this->assertTrue($compiled === $actual);
     }
+
+    /** @test */
+    public function test_hydrates_and_compiles_html_string()
+    {
+        $this->assertTrue(
+            Utils::hydrateAndCompile(__('Click {$here} to enter our new {$webpage} only available until {$time}'),
+            ['here' => 'ENTER', 'webpage' => 'page', 'time' => '00:00']) ===
+            "Click ENTER to enter our new page only available until 00:00"
+        );
+    }
+
 }
